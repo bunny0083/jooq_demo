f310dd563114e995660a324fcb9f84f442668094
Connor Su <bunny0083@gmail.com>
2023-07-27 21:24:09 +0800

    init repository

---
+++ src/main/java/com/example/jooq/JooqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JooqDemoApplication.class, args);
+  }
+
+}
